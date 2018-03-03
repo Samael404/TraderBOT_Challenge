3830c9528e041f65968dd8f1aaf9ea7e8b723662
john <email@example.com>
2018-03-03 15:37:34 -0700

    modifying js script to add routing

--- bot-app/src/components/header.js
+++ bot-app/src/components/header.js
@@ -1,41 +1,17 @@
 import React, { Component } from "react";
-import {
-    Route,
-    Link,
-    HashRouter,
-} from 'react-router-dom';
-import About from "./About";
-import History from "./History";
-import Challange from "./Challange";
-import Price from "./Price";
-import LeaderBoard from "./LeaderBoard";
-
-
-
+import { Route, Link, BrowserRouter } from 'react-router-dom';
 
 class Header extends Component {
     render() {
         return(
-            <HashRouter>
-                <div>
-                    <h1>TraderBot_Challange</h1>
-                    <ul className="header">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/About">About</Link></li>
-                        <li><Link to="/History">History</Link></li>
-                        <li><Link to="/Challange">Challange</Link></li>
-                        <li><Link to="/Price">Price</Link></li>
-                        <li><Link to="/LeaderBoard">Leaderboard</Link></li>
-                    </ul>
-                    <div className="content">
-                        <Route path="/About" component={About}/>
-                        <Route path="/History" component={History}/>
-                        <Route path="/Challange" component={Challange}/>
-                        <Route path="/Price" component={Price}/>
-                        <Route path="/LeaderBoard" component={LeaderBoard}/> 
-                    </div>           
-                </div>
-            </HashRouter>   
+            <div>
+                <h1>TraderBot_Challange</h1>
+                <ul className="header">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/Page1">Page1</Link></li>
+                    <li><Link to="/Page2">Page2</Link></li>
+                </ul>
+            </div>
         );
     }
 }
