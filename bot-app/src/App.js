19dd51f7e34f34b531351cb1609a7c6ead5c7cc3
john <email@example.com>
2018-03-03 15:37:34 -0700

    modifying js script to add routing

--- bot-app/src/App.js
+++ bot-app/src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+
+import Header from './components/header'
 import './App.css';
-import Header from "./components/header.js";
-import StartButton from "./StartButton.js";
-import Table1 from './Table1';
 
-
+const Home = () => <h1>Home</h1>;
+const Page1 = () => <h1>Page1</h1>;
+const Page2 = () => <h1>Page2</h1>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <StartButton />
-        <br></br>
-        <Table1 />
-      </div>
+        <BrowserRouter>
+            <div>
+                <div className="Navbar">
+                    <Header/>
+                </div>
+            <Route path='/' component={Home}/>
+            <Route path='/page1' component={Page1}/>
+            <Route path='/page2' component={Page2}/>
+            </div>
+        </BrowserRouter>
     );
   }
 }
