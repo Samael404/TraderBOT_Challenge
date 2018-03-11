f995c5f298fa950ceb20f69d2f9a6a3507c8c8d8
John Vennard <{ID}+{username}@users.noreply.github.com>
2018-03-11 13:56:16 -0600

    cleaning up branch before merge to master

--- connector_api.py
+++ connector_api.py
@@ -28,20 +28,12 @@ def bot_info(user):
 def bot_launch(user, bot_name):
     ''' launch a bot '''
     output = {'user': user, 'bot_name': bot_name, 'launch': True}
-    # TODO: Suggestion to utilize importlib functionality to dynamically try and load path of python bot
-    # - then your requirement will be to have the same hook in calls for every bot (aka a template file will be needed)
-    # - function will return failure if the bot hasn't been found - otherwise always a success
-    # - will have to launch a async thread for bot and check back on a regular basis for results
-    # - which means this function will be responsible for writing to the database
-    # - suggestion to manage extra threads by storing bot_name, user, and pid in this api - that way if we could check if a process is still running
-    # - simpler method would be to just have another api call for the bots to make but that is sloppier long term
     return json.dumps(output)
 
 @app.route("/scores")
 def score_data():
     ''' returns db information on all bot run statistics ''' 
     output = [{'id': 0, 'user': 'test', 'bot_name': 'botA', 'bot_version': 'v1.0', 'init_total': 1000.00, 'init_final': 2000.00, 'trades_made': 5}] 
-    # TODO: Must query db for real data
     return json.dumps(output) 
 
 if __name__ == '__main__':
