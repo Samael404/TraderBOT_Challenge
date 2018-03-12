df59d60f147dee46397069e5259ed465e32498b7
Kevin Garrett <kevingarrett.404@gmail.com>
2018-03-12 10:55:11 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # Requires python3 and pip3
+# Run with 'sudo -H'
+# Baseline is in place. Can get fancy with the shell variables later to actually modify the pw set on the sql connection
+
+apt-get update && apt-get upgrade -y
+apt-get install -y python3-pip
+
+#May need for future cloud deploy
+#apt-get install -y python3-venv 
 
 echo "Must run with sudo -H"
 
@@ -9,4 +17,6 @@ pip3 install -U flask-cors
 pip3 install -U mysql-connector
 
 echo "Setting up DB"
-apt-get install mysql-server
+echo "mysql-server-5.6 mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get -y install mysql-server-5.6
