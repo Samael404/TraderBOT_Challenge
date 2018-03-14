8573508b68a93d5172747f0800550f6082ea6bab
Kevin Garrett <kevingarrett.404@gmail.com>
2018-03-13 19:34:38 -0700

    2

--- connector_api.py
+++ connector_api.py
@@ -64,7 +64,7 @@ def login(user):
         cursor.execute(query, user)
         row = cursor.fetchone()
         print(row)
-        if str(row) != 'None':
+        if str(row) != None:
             print("Username found.")
         else:
             print("Not sure what to do about this.")
