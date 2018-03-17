dcd94a3dbcbe4daf2eeda7a68d62f6c96e9633e4
wiggro420 <testing@gmail.com>
2018-03-17 12:47:17 -0600

    got functional button

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
 class App extends Component {
+
+  myfunc(){
+    console.log("got here");
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="Button">Button
+          <button onClick={this.myfunc}/>
+        </div>
+        <div>
+
+        </div>
       </div>
+
     );
   }
 }
