744f25263b12038d83a25922a0878c226cca43bc
wiggro420 <testing@gmail.com>
2018-03-17 15:28:43 -0600

    working on getting fetch to return json to app

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -4,23 +4,43 @@ import './App.css';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      'key': '',
 
+    }
+  }
   myfunc(){
     console.log("got here");
     fetch('http://localhost:5000/login/josh', {
       mode: "no-cors",
       method: "GET",
       headers: {
-        "Accept": "application/json"
+       "Content-type": "application/json"
       }
     })
+    //.then(function(res){console.log(res.json())})
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      console.log(responseJSON);
+    });
+    
+    /*
     .then(results => {
       console.log(results);
       return results.json();
     })
     .then(results => {
       console.log(results);
+    })
+    .catch(function(error){
+      console.log(error);
     });
+    this.setState({
+      'key': 'testing123'
+    }) */
+
   };
 
   
@@ -29,10 +49,10 @@ class App extends Component {
     return (
       <div>
         <div className="Button">Button
-          <button onClick={this.myfunc}/>
+          <button onClick={() => this.myfunc()}/>
         </div>
         <div>
-
+          {this.state.key}
         </div>
       </div>
 
