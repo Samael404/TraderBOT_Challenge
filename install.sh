baaa6ced8f1fce4a1260d494188a9060c9c5382c
Kevin Garrett <kevingarrett.404@gmail.com>
2018-03-17 14:12:38 -0700

    Update to shell setup

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # Run with 'sudo -H'
 # Baseline is in place. Can get fancy with the shell variables later to actually modify the pw set on the sql connection
 
-apt-get update && apt-get upgrade -y
-apt-get install -y python3-pip
+#apt-get update && apt-get upgrade -y
+#apt-get install -y python3-pip
 
 #May need for future cloud deploy
 #apt-get install -y python3-venv 
@@ -12,11 +12,19 @@ apt-get install -y python3-pip
 echo "Must run with sudo -H"
 
 # Installing python flask API dependencies
-pip3 install -U flask
-pip3 install -U flask-cors
-pip3 install -U mysql-connector
+#pip3 install -U flask
+#pip3 install -U flask-cors
+#pip3 install -U mysql-connector
 
 echo "Setting up DB"
-echo "mysql-server-5.6 mysql-server/root_password password root" | sudo debconf-set-selections
-echo "mysql-server-5.6 mysql-server/root_password_again password root" | sudo debconf-set-selections
-apt-get -y install mysql-server-5.6
+echo Please enter a password for your local database:
+
+read varname 
+
+echo $varname > .sql_creds
+#Not sure about the following 2 echo lines
+echo "mysql-server mysql-server/root_password password $varname" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $varname" | sudo debconf-set-selections
+apt-get -y install mysql-server
+
+echo -e "$varname\nn\nY\nY\nY\nY\n" | mysql_secure_installation
